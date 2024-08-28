5866074407731d426f8fe3a38ed312b50b73dc52
jasonhsu0212 <gn0039625@crepowermay>
2024-08-28 10:52:58 +0800

    add 部門與員工model關聯

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Employee extends Authenticatable
 {
     use HasFactory, Notifiable;
 
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'dep_code',
     ];
 
     /**
@@ -43,5 +44,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'dep_code');
     }
 }
